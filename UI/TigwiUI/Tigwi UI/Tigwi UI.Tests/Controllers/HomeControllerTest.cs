1b2f8fa472ea63554a063f4a3a5b5efaf7344f8b
Basile Clément <basile.clement@ens.fr>
2012-03-08 14:51:01 +0100

    Created a VS project.

---
+++ UI/TigwiUI/Tigwi UI/Tigwi UI.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using NUnit.Framework;
+using Tigwi_UI;
+using Tigwi_UI.Controllers;
+
+namespace Tigwi_UI.Tests.Controllers
+{
+    [TestFixture]
+    public class HomeControllerTest
+    {
+        [Test]
+        public void Index()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.That("Welcome to ASP.NET MVC!", Is.EqualTo(result.ViewBag.Message));
+        }
+
+        [Test]
+        public void About()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.About() as ViewResult;
+
+            // Assert
+            Assert.That(result, Is.Not.Null);
+        }
+    }
+}
---
+++ UI/TigwiUI/Tigwi UI/Tigwi UI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Tigwi_UI.MvcApplication" Language="C#" %>
