9b6415d21a52fcab2eb9b88e3b6f6a5947b94539
Thomas Williams <thomas.williams@ens.fr>
2012-03-14 20:58:16 +0100

    Test project (forms, session, storage)

---
+++ MvcApplication2/MvcApplication2/Controllers/AuthController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication2.Controllers
+{
+    public class AuthController : Controller
+    {
+        //
+        // GET: /Auth/Login/ ?id=3
+        public ActionResult Login()
+        {
+            ViewData["Test"] = Session["Username"];
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(string login, string password)
+        {
+            Session["Username"] = login;
+            ViewData["Test"] = "Foo";
+            return View();
+        }
+
+        public ActionResult LogOff()
+        {
+            Session.Remove("Username");
+            return RedirectToAction("Login");
+        }
+    }
+}
---
+++ MvcApplication2/MvcApplication2/Models/Auth.cs
@@ -0,0 +1,12 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcApplication2.Models
+{
+    public class AuthMethod 
+    {
+    }
+}
