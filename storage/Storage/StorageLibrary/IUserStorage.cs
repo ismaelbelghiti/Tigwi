739f8944fe02c3d2f4708f8bf64670b1a317f610
Florian Bourse <florian.bourse@ens.fr>
2012-03-21 23:03:53 +0100

    fin des interfaces avec deux classes pour les info des users et des accounts

---
+++ storage/Storage/StorageLibrary/IUserStorage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StorageLibrary
+{
+    interface IUserStorage
+    {
+        UserInfo Get_user_info(int user_id);
+        int Get_user_id(string login);
+        void Set_user_info(int user_id, UserInfo new_info);
+        /// <summary>
+        /// Return the id of the new user
+        /// </summary>
+        /// <param name="info"></param>
+        /// <returns></returns>
+        int Create_user(UserInfo info);
+        void Delete_user(int user_id);
+    }
+}
