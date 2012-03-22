c6101946aced8290060e298b977fdb8ba9ca2c93
unknown <ulysse.beaugnon@ulysse_portable.(none)>
2012-03-22 13:05:25 +0100

    Modification de l'interface

--- storage/Storage/StorageLibrary/IMsgStorage.cs
+++ storage/Storage/StorageLibrary/IMsgStorage.cs
@@ -7,27 +7,25 @@ namespace StorageLibrary
 {
     interface IMsgStorage
     {
-        HashSet<int> Get_accounts_msg_from(HashSet<int> accounts, int first_msg, int msg_number);
-        HashSet<int> Get_lists_msg_from(HashSet<int> lists, int first_msg, int msg_number);
-        HashSet<int> Get_accounts_msg_to(HashSet<int> accounts, int last_msg, int msg_number);
-        HashSet<int> Get_lists_msg_to(HashSet<int> lists, int last_msg, int msg_number);
+        List<IMessage> Get_accounts_msg_from(HashSet<int> accounts, int first_msg, int msg_number);
+        List<IMessage> Get_lists_msg_from(HashSet<int> lists, int first_msg, int msg_number);
+        List<IMessage> Get_accounts_msg_to(HashSet<int> accounts, int last_msg, int msg_number);
+        List<IMessage> Get_lists_msg_to(HashSet<int> lists, int last_msg, int msg_number);
         void Tag_msg(int account_id, int msg_id);
         void Untag_msg(int accound_id, int msg_id);
-        HashSet<int> Get_tagged_msg_from(int accound_id, int first_msg, int msg_number);
-        HashSet<int> Get_tagged_msg_to(int account_id, int last_msg, int msg_number);
+        List<IMessage> Get_tagged_msg_from(int accound_id, int first_msg, int msg_number);
+        List<IMessage> Get_tagged_msg_to(int account_id, int last_msg, int msg_number);
         /// <summary>
-        /// Return the id of the new message
         /// </summary>
         /// <param name="account_id"></param>
         /// <param name="msg"></param>
-        /// <returns></returns>
+        /// <returns>the id of the new message</returns>
         int Post_msg(int account_id, string msg);
         /// <summary>
-        /// Return the id of the new message
         /// </summary>
         /// <param name="accound_id"></param>
         /// <param name="msg_id"></param>
-        /// <returns></returns>
+        /// <returns> id of the new message</returns>
         int Copy_msg(int accound_id, int msg_id);
         void Remove_msg(int msg_id);
     }
