f9a3b3765766235cee05f7aa0bd7bd8fbd5643ca
Basile Clément <basile.clement@ens.fr>
2012-03-28 18:46:14 +0200

    Inserted greetings about Tigwi

--- UI/TigwiUI/Tigwi UI/Tigwi UI/Controllers/HomeController.cs
+++ UI/TigwiUI/Tigwi UI/Tigwi UI/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace Tigwi_UI.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Welcome to ASP.NET MVC!";
+            ViewBag.Message = "Welcome to the home page of Tigwi !";
 
             return View();
         }
