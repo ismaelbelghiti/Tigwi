97369af6a127199f23f492d7eceab5d11b376077
unknown <ulysse.beaugnon@ulysse_portable.(none)>
2012-03-29 14:59:22 +0200

    creation de la queue

--- storage/Storage/StorageLibrary/interfaces/IUserStorage.cs
+++ storage/Storage/StorageLibrary/interfaces/IUserStorage.cs
@@ -7,12 +7,41 @@ namespace StorageLibrary
 {
     public interface IUserStorage
     {
+        /// <summary>
+        /// Can raise : 
+        /// UserNotFound
+        /// </summary>
         int GetId(string login);
+
+        /// <summary>
+        /// Can raise : 
+        /// UserNotFound
+        /// </summary>
         IUserInfo GetInfo(int userId);
+
+        /// <summary>
+        /// Can raise : 
+        /// UserNotFound
+        /// UserAlreadyExists
+        /// </summary>
         void SetInfo(int userId, string login, string email);
 
+        /// <summary>
+        /// Can raise : 
+        /// UserNotFound
+        /// </summary>
         HashSet<int> GetAccounts(int userId);
+
+        /// <summary>
+        /// Can raise : 
+        /// UserAlreadyExists
+        /// </summary>
         int Create(string login, string email);
+       
+        /// <summary>
+        /// Dont do anything if the user is already deleted
+        /// The user might not be deleted immediately
+        /// </summary>
         void Delete(int userId);
     }
 }
