4e65b04a8eb3eae5263fb87e67d17685ae00e28e
Zebign1 <leonard.de.haro@ens.fr>
2012-04-01 14:14:08 +0200

    update API Global.asax and ApiController

--- API/Tigwi-API/Global.asax.cs
+++ API/Tigwi-API/Global.asax.cs
@@ -1,34 +1,71 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿
+// Les routes pour transmettre les informations aux controllers ;
 using System.Web.Mvc;
 using System.Web.Routing;
 
-namespace Tigwi_API
+namespace Tigwi_API.Routes
 {
-    // Remarque : pour obtenir des instructions sur l'activation du mode classique IIS6 ou IIS7, 
-    // visitez http://go.microsoft.com/?LinkId=9394801
-
     public class MvcApplication : System.Web.HttpApplication
     {
         public static void RegisterRoutes(RouteCollection routes)
-        {
-            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+		{
+			routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+			// customer routes
 
-            routes.MapRoute(
-                "Default", // Nom d'itinéraire
-                "{controller}/{action}/{id}", // URL avec des paramètres
-                new { controller = "Home", action = "Index", id = UrlParameter.Optional } // Paramètres par défaut
-            );
+			routes.MapRoute("Message", 
+				"ApiController/UserTimeline/{name}/{numberOfMessages}",
+				new { controller = "ApiController", 
+					action = "UserTimeline", 
+					name="", 
+					numberOfMessages="20"} // Par défaut le nombre de messages est 20
+				);
 
-        }
+			routes.MapRoute("Users", 
+				"ApiController/UserSubscriptionsList/{name}/{numberOfSubscriptions}",
+				new { controller = "ApiController", 
+					action = "UserSubscriptionsList", 
+					name="",
+					numberOfSubscriptions="20"}
+				); 
+
+			routes.MapRoute("Users",
+				"ApiController/UserSubscribersList/{name}/{numberOfSubscribers}",
+				new { controller = "ApiController", 
+					action = "UserSubscribersList", 
+					name="",
+					numberOfSubscribers="20"}
+				);
+			
+			routes.MapRoute("Message",
+				"ApiController/WritePost/{name}/{message}",
+				new { controller = "ApiController", 
+					action = "WritePost", 
+					name="",
+					message=""}
+				);
+		
+			routes.MapRoute("Users",
+				"ApiController/Suscribe/{follower}/{followed}",
+				new { controller = "ApiController", 
+					action = "Suscribe", 
+					follower="",
+					followed=""}
+				);
+
+			// default route
+			routes.MapRoute("Default", "{controller}/{action}/{id}",
+				new { controller = "Home", action = "Index", id = ""});
+		}
 
         protected void Application_Start()
         {
-            AreaRegistration.RegisterAllAreas();
-
             RegisterRoutes(RouteTable.Routes);
         }
     }
 }
+
+
+
+
+
+
