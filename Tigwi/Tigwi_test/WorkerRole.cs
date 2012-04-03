518538e4f1aae2f5a4c2f5ae83d956c8f6f672e4
Florian Bourse <florian.bourse@ens.fr>
2012-04-03 22:03:28 +0200

    tests storage

--- Tigwi/Tigwi_test/WorkerRole.cs
+++ Tigwi/Tigwi_test/WorkerRole.cs
@@ -8,6 +8,7 @@ using Microsoft.WindowsAzure;
 using Microsoft.WindowsAzure.Diagnostics;
 using Microsoft.WindowsAzure.ServiceRuntime;
 using Microsoft.WindowsAzure.StorageClient;
+using StorageLibrary;
 
 namespace Tigwi_test
 {
@@ -19,6 +20,63 @@ namespace Tigwi_test
             Trace.WriteLine("$projectname$ entry point called", "Information");
 
             // Test code here
+            Storage storage = new Storage("eyaris", "StorageConnectionString");
+
+            UserStorage userstorage = new UserStorage(storage.connexion);
+            AccountStorage accountstorage = new AccountStorage(storage.connexion);
+
+            Guid userid = userstorage.Create("test", "bidon@test.com");
+            Guid sameid = userstorage.GetId("test");
+            if (userid != sameid) Trace.WriteLine("Error, User.GetId");
+
+            IUserInfo userinfo = userstorage.GetInfo(userid);
+            if (userinfo.Login != "test" || userinfo.Email != "bidon@test.com")
+                Trace.WriteLine("Error, User.GetInfo");
+
+            userstorage.SetInfo(userid, "settest", "email@test.com");
+            userinfo = userstorage.GetInfo(userid);
+            if (userinfo.Login != "settest" || userinfo.Email != "email@test.com")
+                Trace.WriteLine("Error, User.SetInfo");
+
+            Guid accountid = accountstorage.Create(userid, "test", "description");
+            sameid = accountstorage.GetId("test");
+            if (accountid != sameid) Trace.WriteLine("Error, Account.GetId");
+
+            IAccountInfo accountinfo = accountstorage.GetInfo(accountid);
+            if (accountinfo.Name != "test" || accountinfo.Description != "description")
+                Trace.WriteLine("Error, Account.GetInfo");
+
+            accountstorage.SetInfo(accountid, "nametest", "descriptiontest");
+            accountinfo = accountstorage.GetInfo(accountid);
+            if (accountinfo.Name != "nametest" || accountinfo.Description != "descriptiontest")
+                Trace.WriteLine("Error, Account.SetInfo");
+
+            HashSet<Guid> accounts = userstorage.GetAccounts(userid);
+            if (! accounts.Contains(accountid))
+                Trace.WriteLine("Error, User.GetAccounts");
+
+            HashSet<Guid> users = accountstorage.GetUsers(accountid);
+            if (! users.Contains(userid))
+                Trace.WriteLine("Error, Account.GetUsers");
+
+            Guid useridbis = userstorage.Create("user2", "user@mail.com");
+            accountstorage.Add(accountid, useridbis);
+            if (!users.Contains(useridbis))
+                Trace.WriteLine("Error, Account.Add");
+
+            Guid admin = accountstorage.GetAdminId(accountid);
+            if (admin != userid)
+                Trace.WriteLine("Error, Account.GetAdmin");
+
+            accountstorage.SetAdminId(accountid, useridbis);
+            admin = accountstorage.GetAdminId(accountid);
+            if (admin != useridbis)
+                Trace.WriteLine("Error, Account.SetAdmin");
+
+            accountstorage.Remove(accountid, userid);
+            accountstorage.Delete(accountid);
+            userstorage.Delete(userid);
+            userstorage.Delete(useridbis);
 
             // End test code
 
