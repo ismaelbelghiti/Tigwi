3aa78b2fdb9411fde4ff911a282c0c3d9c687467
Ulysse <ulysse.beaugnon@ens.fr>
2012-04-05 17:21:08 +0200

    ajout d'un projet pour faire des tests en local

---
+++ Tigwi/StorageLocalTest/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using StorageCommon;
+using StorageLibrary;
+using Microsoft.WindowsAzure.StorageClient;
+
+namespace StorageLocalTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Storage storage = new Storage("ulyssestorage", "7VkCf60UV8e/OaDAfCixFOSqHQKWm4gZTgsyMhOrdmfgEmeyqoLnL52KLoYv/9zJ+zRmdd2D3uJgD7EArE6byg==");
+            Mutex m1 = new Mutex(storage.connexion.userContainer, "lock/1");
+            Mutex m2 = new Mutex(storage.connexion.userContainer, "lock/1");
+
+            Console.WriteLine("Init ended");
+            m1.Acquire();
+            Console.WriteLine("m1 ok");
+            m2.Acquire();
+            Console.WriteLine("m2 ok");
+        }
+    }
+}
