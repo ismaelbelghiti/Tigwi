917283a4f3a418bd0066af6c2e15ba1f5018135b
Ulysse <ulysse.beaugnon@ens.fr>
2012-04-07 16:03:32 +0200

    ajout de path

---
+++ storage/Storage/StorageLibrary/datastructure/Path.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StorageLibrary
+{
+    class Path
+    {
+        public const string U_INFO = "info/";
+        public const string U_ACCOUNTS = "accounts/";
+        public const string U_IDBYLOGIN = "idbylogin/";
+        public const string U_ACC_DATA = "/data";
+        public const string U_ACC_LOCK = "/lock";
+    }
+}
