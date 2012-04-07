cc55382665c9ad214dbaa34da9712a685ddd2c8b
Ulysse <ulysse.beaugnon@ens.fr>
2012-04-07 19:08:54 +0200

    add atomic add operation on hashset

---
+++ Tigwi/StorageCommon/HashSetBlob.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.WindowsAzure.StorageClient;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace StorageCommon
+{
+    public class HashSetBlob<T>
+    {
+        CloudBlob blob;
+        BinaryFormatter formatter;
+
+        public HashSetBlob(CloudBlobContainer container, string blobName)
+        {
+            blob = container.GetBlobReference(blobName);
+            formatter = new BinaryFormatter();
+        }
+
+        public bool Add(T item)
+        {
+            BlobRequestOptions reqOpt = new BlobRequestOptions();
+            HashSet<T> set;
+            BlobStream stream;
+            string eTag;
+            bool keepGoing;
+
+            do
+            {
+                try
+                {
+                    eTag = blob.Attributes.Properties.ETag;
+                    stream = blob.OpenRead();
+                    set = (HashSet<T>)formatter.Deserialize(stream);
+                    stream.Close();
+                }
+                catch
+                {
+                    return false;
+                }
+
+                set.Add(item);
+                reqOpt.AccessCondition = AccessCondition.IfMatch(eTag);
+
+                try
+                {
+                    stream = blob.OpenWrite(reqOpt);
+                    formatter.Serialize(stream, set);
+                    stream.Close();
+                    keepGoing = false;
+                }
+                catch(Exception)
+                {
+                    keepGoing = true;
+                }
+
+            } while (keepGoing);
+
+            return true;
+        }
+    }
+}
