1cfd3738a92694717786cfb106a47da37c93c45e
Basile Clément <basile.clement@ens.fr>
2012-04-10 14:50:22 +0200

    Beginning of the architecture of the UI part.

--- Tigwi/Tigwi.UI/Controllers/AccountController.cs
+++ Tigwi/Tigwi.UI/Controllers/AccountController.cs
@@ -11,183 +11,102 @@ namespace Tigwi.UI.Controllers
 {
     public class AccountController : Controller
     {
-
-        //
-        // GET: /Account/LogOn
-
-        public ActionResult LogOn()
+        /// <summary>
+        /// Shows a page listing all the accounts of the active user.
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult List()
         {
-            return View();
+            throw new NotImplementedException("AccountController.List");
         }
 
-        //
-        // POST: /Account/LogOn
+        /// <summary>
+        /// Makes the given account active (the one which will post things by default, etc.)
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult MakeActive()
+        {
+            throw new NotImplementedException("AccountController.MakeActive");
+        }
 
+        /// <summary>
+        /// Shows a form for creating a new account associated with the active user.
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Create()
+        {
+            throw new NotImplementedException("AccountController.Create");
+        }
+
+        /// <summary>
+        /// Creates a new account associated with the active user (i.e. the active user will
+        /// be the administrator and sole member of the account).
+        /// </summary>
+        /// <param name="accountCreation"></param>
+        /// <returns></returns>
         [HttpPost]
-        public ActionResult LogOn(LogOnModel model, string returnUrl)
+        public ActionResult Create(AccountCreationModel accountCreation)
         {
-            if (ModelState.IsValid)
-            {
-                if (Membership.ValidateUser(model.UserName, model.Password))
-                {
-                    FormsAuthentication.SetAuthCookie(model.UserName, model.RememberMe);
-                    if (Url.IsLocalUrl(returnUrl) && returnUrl.Length > 1 && returnUrl.StartsWith("/")
-                        && !returnUrl.StartsWith("//") && !returnUrl.StartsWith("/\\"))
-                    {
-                        return Redirect(returnUrl);
-                    }
-                    else
-                    {
-                        return RedirectToAction("Index", "Home");
-                    }
-                }
-                else
-                {
-                    ModelState.AddModelError(string.Empty, "The user name or password provided is incorrect.");
-                }
-            }
-
-            // If we got this far, something failed, redisplay form
-            return View(model);
+            throw new NotImplementedException("AccountController.Create[POST]");
         }
 
-        //
-        // GET: /Account/LogOff
-
-        public ActionResult LogOff()
+        /// <summary>
+        /// Show a form to edit the given account.
+        /// If no account is given, edit the active account instead.
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Edit()
         {
-            FormsAuthentication.SignOut();
-
-            return RedirectToAction("Index", "Home");
+            throw new NotImplementedException("AccountController.Edit");
         }
 
-        //
-        // GET: /Account/Register
-
-        public ActionResult Register()
-        {
-            return View();
-        }
-
-        //
-        // POST: /Account/Register
-
+        /// <summary>
+        /// Updates the given account's parameters.
+        /// </summary>
+        /// <param name="accountEdit"></param>
+        /// <returns></returns>
         [HttpPost]
-        public ActionResult Register(RegisterModel model)
+        public ActionResult Edit(AccountEditModel accountEdit)
         {
-            if (ModelState.IsValid)
-            {
-                // Attempt to register the user
-                MembershipCreateStatus createStatus;
-                Membership.CreateUser(model.UserName, model.Password, model.Email, null, null, true, null, out createStatus);
-
-                if (createStatus == MembershipCreateStatus.Success)
-                {
-                    FormsAuthentication.SetAuthCookie(model.UserName, false /* createPersistentCookie */);
-                    return RedirectToAction("Index", "Home");
-                }
-                else
-                {
-                    ModelState.AddModelError(string.Empty, ErrorCodeToString(createStatus));
-                }
-            }
-
-            // If we got this far, something failed, redisplay form
-            return View(model);
+            throw new NotImplementedException("AccountController.Edit");
         }
 
-        //
-        // GET: /Account/ChangePassword
-
-        [Authorize]
-        public ActionResult ChangePassword()
+        /// <summary>
+        /// Show all the people an account is following.
+        /// </summary>
+        /// <returns>The resulting view.</returns>
+        public ActionResult Following()
         {
-            return View();
+            throw new NotImplementedException("AccountController.Following");
         }
 
-        //
-        // POST: /Account/ChangePassword
-
-        [Authorize]
-        [HttpPost]
-        public ActionResult ChangePassword(ChangePasswordModel model)
+        /// <summary>
+        /// Makes the active account follow the given account.
+        /// Idempotent.
+        /// </summary>
+        /// <returns>The resulting view.</returns>
+        public ActionResult Follow()
         {
-            if (ModelState.IsValid)
-            {
-
-                // ChangePassword will throw an exception rather
-                // than return false in certain failure scenarios.
-                bool changePasswordSucceeded;
-                try
-                {
-                    MembershipUser currentUser = Membership.GetUser(User.Identity.Name, true /* userIsOnline */);
-                    changePasswordSucceeded = currentUser.ChangePassword(model.OldPassword, model.NewPassword);
-                }
-                catch (Exception)
-                {
-                    changePasswordSucceeded = false;
-                }
-
-                if (changePasswordSucceeded)
-                {
-                    return RedirectToAction("ChangePasswordSuccess");
-                }
-                else
-                {
-                    ModelState.AddModelError(string.Empty, "The current password is incorrect or the new password is invalid.");
-                }
-            }
-
-            // If we got this far, something failed, redisplay form
-            return View(model);
+            throw new NotImplementedException("AccountController.Follow");
         }
 
-        //
-        // GET: /Account/ChangePasswordSuccess
-
-        public ActionResult ChangePasswordSuccess()
+        /// <summary>
+        /// Makes the active account unfollow the given account.
+        /// Idempotent.
+        /// </summary>
+        /// <returns>The resulting view.</returns>
+        public ActionResult UnFollow()
         {
-            return View();
+            throw new NotImplementedException("AccountController.UnFollow");
         }
 
-        #region Status Codes
-        private static string ErrorCodeToString(MembershipCreateStatus createStatus)
+        /// <summary>
+        /// Shows all the people that follow a given account.
+        /// </summary>
+        /// <returns>The resulting view.</returns>
+        public ActionResult Followers()
         {
-            // See http://go.microsoft.com/fwlink/?LinkID=177550 for
-            // a full list of status codes.
-            switch (createStatus)
-            {
-                case MembershipCreateStatus.DuplicateUserName:
-                    return "User name already exists. Please enter a different user name.";
-
-                case MembershipCreateStatus.DuplicateEmail:
-                    return "A user name for that e-mail address already exists. Please enter a different e-mail address.";
-
-                case MembershipCreateStatus.InvalidPassword:
-                    return "The password provided is invalid. Please enter a valid password value.";
-
-                case MembershipCreateStatus.InvalidEmail:
-                    return "The e-mail address provided is invalid. Please check the value and try again.";
-
-                case MembershipCreateStatus.InvalidAnswer:
-                    return "The password retrieval answer provided is invalid. Please check the value and try again.";
-
-                case MembershipCreateStatus.InvalidQuestion:
-                    return "The password retrieval question provided is invalid. Please check the value and try again.";
-
-                case MembershipCreateStatus.InvalidUserName:
-                    return "The user name provided is invalid. Please check the value and try again.";
-
-                case MembershipCreateStatus.ProviderError:
-                    return "The authentication provider returned an error. Please verify your entry and try again. If the problem persists, please contact your system administrator.";
-
-                case MembershipCreateStatus.UserRejected:
-                    return "The user creation request has been canceled. Please verify your entry and try again. If the problem persists, please contact your system administrator.";
-
-                default:
-                    return "An unknown error occurred. Please verify your entry and try again. If the problem persists, please contact your system administrator.";
-            }
+            throw new NotImplementedException("AccountController.Followers");
         }
-        #endregion
     }
 }
---
+++ Tigwi/Tigwi.UI/Controllers/PostController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Tigwi.UI.Controllers
+{
+    public class PostController : Controller
+    {
+        /// <summary>
+        /// Tags (favorites) a post.
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Tag()
+        {
+            throw new NotImplementedException("PostController.Tag");
+        }
+
+        /// <summary>
+        /// Untag (unfavorites) a post.
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult UnTag()
+        {
+            throw new NotImplementedException("PostController.UnTag");
+        }
+
+        /// <summary>
+        /// Write a new post.
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Write()
+        {
+            throw new NotImplementedException("PostController.Write");
+        }
+
+        /// <summary>
+        /// Deletes a post.
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Delete()
+        {
+            throw new NotImplementedException("PostController.Delete");
+        }
+
+        /// <summary>
+        /// Reply to a  post. The difference with <c>Write</c> is that it could be better integrated in the UI
+        /// (see twitter).
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Reply()
+        {
+            throw new NotImplementedException("PostController.Reply");
+        }
+
+        /// <summary>
+        /// Repost something poster by another user (Retweets in Twitter).
+        /// </summary>
+        /// <returns></returns>
+        /// TODO: Find another name. The term "Repost" has a negative connotation.
+        public ActionResult Repost()
+        {
+            throw new NotImplementedException("PostController.Repost");
+        }
+    }
+}
