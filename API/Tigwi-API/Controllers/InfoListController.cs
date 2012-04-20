7712791b83c330e543d9b944e518fbcc99f7a416
unknown <Utilisateur@Utilisateur-PC.(none)>
2012-04-20 21:33:32 +0200

    infoListController

--- API/Tigwi-API/Controllers/InfoListController.cs
+++ API/Tigwi-API/Controllers/InfoListController.cs
@@ -16,8 +16,27 @@ namespace Tigwi_API.Controllers
 
         public ActionResult Subscriptions(Guid idOfList, int number)
         {
-            //TODO: Implement this
-            throw new NotImplementedException();
+            Answer output;
+
+            try
+            {
+                // get accounts following a given list 
+                list listFollowedAccounts = storage.List.GetAccounts(idOfList);
+                var numberToReturn = Math.Min(number, listFollowedAccounts.count);
+                List<user> listFollowedAccountsOutput = listFollowedAccounts.GetRange(numberToReturn, 1);
+
+                output = new Answer(listFollowedAccountsOutput);
+            }
+
+            catch (StorageLibException exception)
+            {
+                output = new Answer(new Error(exception.Code.ToString()));
+            }
+
+            var stream = new MemoryStream();
+            (new XmlSerializer(typeof(Answer))).Serialize(stream, output);
+
+            return Content(stream.ToString());
         }
 
         //
@@ -25,17 +44,53 @@ namespace Tigwi_API.Controllers
 
         public ActionResult Subscribers(Guid idOfList, int number)
         {
-            //TODO: Implement this
-            throw new NotImplementedException();
+            Answer output;
+
+            try
+            {
+                // get accounts following a given list 
+                list listSuscribersAccount = storage.List.GetFollowingAccounts(idOfList);
+                var numberToReturn = Math.Min(number, listSuscribersAccount.count);
+                List<user> listSuscribersOutput = listSuscribersAccount.GetRange(numberToReturn, 1);
+
+                output = new Answer(listSuscribersOutput);
+            }
+            catch (StorageLibException exception)
+            {
+                output = new Answer(new Error(exception.Code.ToString()));
+            }
+
+            var stream = new MemoryStream();
+            (new XmlSerializer(typeof(Answer))).Serialize(stream, output);
+
+            return Content(stream.ToString());
         }
+
 
         //
         // GET : /infolist/owner/{idOfList}
 
         public ActionResult Owner(Guid idOfList)
         {
-            //TODO: implement this
-            throw new NotImplementedException();
+            Answer output;
+
+            try
+            {
+                // get accounts following a given list 
+                var owner = storage.List.GetOwner(idOfList);
+
+                output = new Answer(owner);
+            }
+
+            catch (StorageLibException exception)
+            {
+                output = new Answer(new Error(exception.Code.ToString()));
+            }
+
+            var stream = new MemoryStream();
+            (new XmlSerializer(typeof(Answer))).Serialize(stream, output);
+
+            return Content(stream.ToString());
         }
 
         //
@@ -43,8 +98,27 @@ namespace Tigwi_API.Controllers
 
         public ActionResult Messages(Guid idOfList, int number)
         {
-            //TODO: implement this
-            throw new NotImplementedException();
+            Answer output;
+
+            try
+            {
+                // get lasts messages from list defined by idOfList
+                list listMsgs = storage.Msg.GetListsMsgTo(new HashSet<Guid> { idOfList }, DateTime.Now, number);
+
+                // convert, looking forward XML serialization
+                var listMsgsOutput = new Messages(listMsgs, storage);
+
+                output = new Answer(listMsgsOutput);
+            }
+            catch (StorageLibException exception)
+            {
+                output = new Answer(new Error(exception.Code.ToString()));
+            }
+
+            var stream = new MemoryStream();
+            (new XmlSerializer(typeof(Answer))).Serialize(stream, output);
+
+            return Content(stream.ToString());
         }
 
     }
