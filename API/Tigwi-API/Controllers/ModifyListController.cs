fb063f4f88e69a7b1bb9e47a7c522f0110d2c250
unknown <Utilisateur@Utilisateur-PC.(none)>
2012-04-21 17:29:51 +0200

    adding unsuscribeList, followList and unfollowList

--- API/Tigwi-API/Controllers/ModifyListController.cs
+++ API/Tigwi-API/Controllers/ModifyListController.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Web.Mvc;
 using System.Xml.Serialization;
 using Tigwi_API.Models;
@@ -21,6 +22,91 @@ namespace Tigwi_API.Controllers
             {
                 var accountId = Storage.Account.GetId(subscribeAccount.Subscription);
                 Storage.List.Add(subscribeAccount.List, accountId);
+
+                // Result is an empty error XML element
+                error = new Error();
+            }
+            catch (StorageLibException exception)
+            {
+                // Result is an non-empty error XML element
+                error = new Error(exception.Code.ToString());
+            }
+
+            var stream = new MemoryStream();
+            (new XmlSerializer(typeof(Error))).Serialize(stream, error);
+
+            return Content(stream.ToString());
+        }
+
+        //
+        // POST : /modifylist/unsubscribeaccount/
+
+        //[Authorize]
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult UnsubscribeAccount(Guid idOfList, Guid accountId)
+        {
+            Error error;
+
+            try
+            {
+                Storage.List.Add(idOfList, accountId);
+
+                // Result is an empty error XML element
+                error = new Error();
+            }
+            catch (StorageLibException exception)
+            {
+                // Result is an non-empty error XML element
+                error = new Error(exception.Code.ToString());
+            }
+
+            var stream = new MemoryStream();
+            (new XmlSerializer(typeof(Error))).Serialize(stream, error);
+
+            return Content(stream.ToString());
+        }
+
+
+        //
+        // POST : /modifylist/followlist/
+
+        //[Authorize]
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult followList(Guid idOfList, Guid accountId)
+        {
+            Error error;
+
+            try
+            {
+                Storage.List.Follow(idOfList, accountId);
+
+                // Result is an empty error XML element
+                error = new Error();
+            }
+            catch (StorageLibException exception)
+            {
+                // Result is an non-empty error XML element
+                error = new Error(exception.Code.ToString());
+            }
+
+            var stream = new MemoryStream();
+            (new XmlSerializer(typeof(Error))).Serialize(stream, error);
+
+            return Content(stream.ToString());
+        }
+
+        //
+        // POST : /modifylist/unfollowlist/
+
+        //[Authorize]
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult unfollowList(Guid idOfList, Guid accountId)
+        {
+            Error error;
+
+            try
+            {
+                Storage.List.Unfollow(idOfList, accountId);
 
                 // Result is an empty error XML element
                 error = new Error();
