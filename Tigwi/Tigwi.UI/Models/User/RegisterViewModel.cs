6aec7ba9fb7f21be15dc861f389094f97e40e48a
Basile Clément <basile.clement@ens.fr>
2012-04-22 16:39:07 +0200

    [UI] Adds a RequestViewModel ViewModel example.

---
+++ Tigwi/Tigwi.UI/Models/User/RegisterViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Tigwi.UI.Models.User
+{
+    using System.ComponentModel.DataAnnotations;
+    using System.Web.Mvc;
+
+    public class RegisterViewModel
+    {
+        [Key]
+        [Required]
+        public string Login { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [MinLength(8)]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare("Password")]
+        public string ConfirmPassword { get; set; }
+    }
+}
