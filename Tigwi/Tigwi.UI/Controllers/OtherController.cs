42641cbfea1d4ed6bc6a0297333ee47766f11691
Timothee Lacroix <dakeyras.khan@gmail.com>
2012-04-25 17:24:54 +0200

    almost empty controller

---
+++ Tigwi/Tigwi.UI/Controllers/OtherController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Tigwi.UI.Models;
+
+namespace Tigwi.UI.Controllers
+{
+    public class OtherController : Controller
+    {
+        //
+        // GET: /Other/
+
+        public ActionResult Index(string user="Me")
+        {
+
+            return View(new OtherViewModel(user));
+        }
+
+    }
+}
