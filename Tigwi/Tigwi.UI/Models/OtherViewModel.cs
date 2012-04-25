f25140b7ddcc845b6e96c2f12fa94abceaae29d8
Timothee Lacroix <dakeyras.khan@gmail.com>
2012-04-25 17:26:09 +0200

    added some viewModels

---
+++ Tigwi/Tigwi.UI/Models/OtherViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Tigwi.UI.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class OtherViewModel
+    {
+        public OtherViewModel(string user)
+        {
+            accountName = user;
+            posts = new List<PostViewModel>();
+            for (int i=0;i<50;i++)
+            {
+                posts.Add(new PostViewModel(user));
+            }
+        }
+        public List<PostViewModel>posts { get; private set; }
+        public string accountName { get; private set; }
+
+    }
+}
---
+++ Tigwi/Tigwi.UI/Models/PostViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Tigwi.UI.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class PostViewModel
+    {
+        public PostViewModel(string poster)
+        {
+            Poster = poster;
+            Content =
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum aliquet sollicitudin rhoncus. In hac habitasse platea dictumst volutpat.";
+        }
+        public virtual string Poster { get; private set; }
+
+        [DataType(DataType.DateTime)]
+        public virtual DateTime PostingDate { get; private set; }
+
+        [DataType(DataType.MultilineText)]
+        public virtual string Content { get; set; }
+
+        public int color { get; set; }
+    }
+}
