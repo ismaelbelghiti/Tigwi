f03bdd4510767800852370667057edd55d24c669
Théo Zimmermann <theo.zimmermann@ens.fr>
2012-04-28 18:05:53 +0200

    splitting infouser into two controllers (with the new controllers is better) - API

---
+++ API/Tigwi-API/Controllers/InfoUserByIdController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using System.Web.Mvc;
+using System.Xml.Serialization;
+using StorageLibrary;
+using Tigwi_API.Models;
+
+namespace Tigwi_API.Controllers
+{
+    public class InfoUserByIdController : InfoUserController
+    {
+
+        //
+        // GET : /infouser/maininformations/{userId}
+        public ActionResult MainInfo(Guid userId)
+        {
+            Answer output;
+
+            try
+            {
+                output = AnswerMainInfo(userId);
+            }
+
+            catch (StorageLibException exception)
+            {
+                // Result is an non-empty error XML element
+                output = new Answer(new Error(exception.Code.ToString()));
+            }
+
+            // a stream is needed for serialization
+            var stream = new MemoryStream();
+            (new XmlSerializer(typeof(Answer))).Serialize(stream, output);
+
+            return Content(stream.ToString());
+        }
+
+        
+        //
+        // GET : /infouser/authorizedaccounts/{userId}/{number}
+        public ActionResult AuthorizedAccounts(Guid userId, int number)
+        {
+            Answer output;
+
+            try
+            {
+                output = AnswerAuthorizedAccounts(userId, number);
+            }
+
+            catch (StorageLibException exception)
+            {
+                // Result is an non-empty error XML element
+                output = new Answer(new Error(exception.Code.ToString()));
+            }
+
+            // a stream is needed for serialization
+            var stream = new MemoryStream();
+            (new XmlSerializer(typeof(Answer))).Serialize(stream, output);
+
+            return Content(stream.ToString());
+        }
+    }
+}
