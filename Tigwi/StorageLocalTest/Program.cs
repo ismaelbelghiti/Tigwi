afbe14a64507320e2e0c1b62b32672d158ac962f
Ulysse <ulysse.beaugnon@ens.fr>
2012-05-01 19:21:08 +0200

    Creation of an application to initialize the storage

--- Tigwi/StorageLocalTest/Program.cs
+++ Tigwi/StorageLocalTest/Program.cs
@@ -9,6 +9,9 @@ namespace StorageLocalTest
 {
     class Program
     {
+        const string azureAccountName = "ulyssestorage";
+        const string azureAccountKey = "";
+
         static void TestUser(IStorage storage)
         {
             Console.WriteLine("TestUser");
@@ -1011,10 +1014,14 @@ namespace StorageLocalTest
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Init connexions");
-            Storage storage = new Storage("ulyssestorage", "XBfWftDi/T9lWl1ms6WA7wy+aPrR8IcJg3vdHd9MG4qJNafOxQYUrQodmuo/lQTXQzHIPTJBbWsrd6cSd6wSSw==");
-
             Console.WriteLine("Clearing previous data");
+            BlobFactory blobFactory = new BlobFactory(azureAccountName, azureAccountKey);
+            blobFactory.InitStorage();
+
+            Console.WriteLine("Init connexions");
+            Storage storage = new Storage(azureAccountName, azureAccountKey);
+
+            
             ClearContainer(storage.connexion.userContainer);
             ClearContainer(storage.connexion.accountContainer);
             ClearContainer(storage.connexion.listContainer);
---
+++ storage/Storage/StorageLibrary/base/BlobFactory.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.WindowsAzure;
+using Microsoft.WindowsAzure.StorageClient;
+
+namespace StorageLibrary
+{
+    // TODO : change this class to private
+	public class BlobFactory
+	{
+        // Cloud Connection
+        private CloudBlobContainer userContainer;
+        private CloudBlobContainer accountContainer;
+        private CloudBlobContainer listContainer;
+        private CloudBlobContainer msgContainer;
+
+        public BlobFactory(string azureAccountName, string azureKey)
+        {
+            // initialize Azure Account
+            StorageCredentialsAccountAndKey storageKey = new StorageCredentialsAccountAndKey(azureAccountName, azureKey);
+            CloudStorageAccount azureAccount = new CloudStorageAccount(storageKey, true);
+
+            // Create blob containers
+            CloudBlobClient blobClient = azureAccount.CreateCloudBlobClient();
+
+            userContainer = blobClient.GetContainerReference("user");
+            accountContainer = blobClient.GetContainerReference("account");
+            listContainer = blobClient.GetContainerReference("list");
+            msgContainer = blobClient.GetContainerReference("msg");
+        }
+
+        public void InitStorage()
+        {
+            userContainer.CreateIfNotExist();
+            ClearContainer(userContainer);
+
+            accountContainer.CreateIfNotExist();
+            ClearContainer(accountContainer);
+
+            listContainer.CreateIfNotExist();
+            ClearContainer(listContainer);
+
+            msgContainer.CreateIfNotExist();
+            ClearContainer(msgContainer);
+        }
+
+        void ClearContainer(CloudBlobContainer c)
+        {
+            BlobRequestOptions opt = new BlobRequestOptions();
+            opt.UseFlatBlobListing = true;
+            foreach (CloudBlob blob in c.ListBlobs(opt))
+                blob.Delete();
+        }
+	}
+}
