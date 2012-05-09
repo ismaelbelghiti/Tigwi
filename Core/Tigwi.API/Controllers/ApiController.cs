a2b623dd0d4e3d2533d8b38e6250fef4718af521
Théo Zimmermann <theo.zimmermann@ens.fr>
2012-05-09 19:15:18 +0200

    toward authentification in API

--- Core/Tigwi.API/Controllers/ApiController.cs
+++ Core/Tigwi.API/Controllers/ApiController.cs
@@ -15,7 +15,12 @@ namespace Tigwi.API.Controllers
         // Initialize storage when instanciating a controller
         protected ApiController ()
         {
-            Storage = new Storage.Library.Storage("sefero", "GU0GjvcPoXKzDFgBSPFbWmCPQrIRHAT6fholbMnxtteY5vQVgYTcWKk/25i/F4m9MFoGHXNf4oYgeAKo+mFO5Q==");
+            Storage = new Storage.Library.Storage("__AZURE_STORAGE_ACCOUNT_NAME", "__AZURE_STORAGE_ACCOUNT_KEY");
+        }
+
+        protected bool CheckAuthentification (string key)
+        {
+            return true;
         }
 
         protected ContentResult Serialize(Answer output)
