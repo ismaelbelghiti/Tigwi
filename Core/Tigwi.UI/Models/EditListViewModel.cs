cf8011ba68c6601146b0ed8d330977e485b75e18
Timothee Lacroix <dakeyras.khan@gmail.com>
2012-05-11 19:16:27 +0200

    adding a list is now supported

---
+++ Core/Tigwi.UI/Models/EditListViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Tigwi.UI.Models.Storage;
+
+namespace Tigwi.UI.Models
+{
+    public class EditListViewModel
+    {
+        [Required]
+        [StringLength(20)]
+        [Display(Name = "List name")]
+        public string Name { get; set; }
+
+        [Required]
+        public IEnumerable<string> UserIds { get; set; }
+
+    }
+}
