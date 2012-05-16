dc85da62f067f9d33804d2ceb4a90626087b6f9b
Guillaume Bury <guillaume.bury@gmail.com>
2012-05-16 17:36:27 +0200

    Storage.SetUp is now safe.

--- Core/Tigwi.Storage.SetUp/Program.cs
+++ Core/Tigwi.Storage.SetUp/Program.cs
@@ -8,14 +8,29 @@ namespace SetUp
 {
     class Program
     {
-        const string azureAccountName = "";
-        const string azureAccountKey = "";
+        const string azureAccountName = "__AZURE_STORAGE_ACCOUNT_NAME";
+        const string azureAccountKey = "__AZURE_STORAGE_ACCOUNT_KEY";
  
         // Lauch this program to reinit the storage
         static void Main(string[] args)
         {
-            BlobFactory blobFactory = new BlobFactory(azureAccountName, azureAccountKey);
-            blobFactory.InitStorage();
+            Console.WriteLine("Vous êtes sur le point de réinitialiser le compte de storage suivant :");
+            Console.WriteLine("nom de compte : " + azureAccountName);
+            Console.WriteLine("clé du compte : " + azureAccountKey);
+            Console.Write("Etes vous sur de vouloir effacer  le storage ? (o/N)");
+            string answer = Console.ReadLine();
+            if (answer == "o")
+            {
+                BlobFactory blobFactory = new BlobFactory(azureAccountName, azureAccountKey);
+                blobFactory.InitStorage();
+                Console.WriteLine("Le compte storage a été réinitialisé");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("Rien n'a été fait.");
+                Console.ReadKey();
+            }
         }
     }
 }
