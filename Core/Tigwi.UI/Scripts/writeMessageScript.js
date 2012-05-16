f2e701c730ed4bb0b9c39361ac8a6677894d8c78
Timothee Lacroix <dakeyras.khan@gmail.com>
2012-05-17 00:41:30 +0200

    centers everythings, the bunny keeps moving around, I'll fix it... some files renamed, some modal declarations moved, a new script file created.

---
+++ Core/Tigwi.UI/Scripts/writeMessageScript.js
@@ -0,0 +1,24 @@
+﻿$(document).ready(function () {
+    $('#inputMessage').attr("rows", 1);
+    $('#inputButton').hide();
+    $('#inputMessage').focus(function () {
+        $(this).attr("rows", 5);
+        $('#inputButton').show();
+        if (this.value == this.defaultValue) {
+            this.value = '';
+        }
+        if (this.value != this.defaultValue) {
+            this.select();
+        }
+    });
+    $('#inputMessage').blur(function () {
+        if (this.value == this.defaultValue || this.value == '') {
+            $(this).attr("rows", 1);
+            $('#inputButton').hide();
+        }
+    });
+
+    $('#inputButton').click(function () {
+        $('#writeMessageForm').submit();
+    });
+});
